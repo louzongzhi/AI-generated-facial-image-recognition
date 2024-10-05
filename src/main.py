d44589f643e32c35951643d87cfcc62168330392
louzongzhi <2871561809@qq.com>
2024-10-06 00:30:09 +0800

    重构代码以提高模型定义的可读性和可维护性

--- src/main.py
+++ src/main.py
@@ -3,7 +3,13 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from model import self_net
 
-test_data = datasets.ImageFolder(root='./testdata', transform=transforms.ToTensor())
+transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+test_data = datasets.ImageFolder(root='./testdata', transform=transform)
 
 model = self_net()
 model.load_state_dict(torch.load('./src/model.pth'))
