3b9ad23fa39823a1d17ccd038a78aa7c23f3f5d2
louzongzhi <2871561809@qq.com>
2024-10-06 17:34:05 +0800

    重构代码以提高模型定义的可读性和可维护性

---
+++ src/data.py
@@ -0,0 +1,130 @@
+import os
+import random
+import numpy as np
+import torch
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
+from PIL import Image
+
+class RealFakeDataLoader:
+    def __init__(self, data_path, cropSize, batch_size, num_threads, isTrain=True):
+        self.data_path = data_path
+        self.cropSize = cropSize
+        self.batch_size = batch_size
+        self.num_threads = num_threads
+        self.isTrain = isTrain
+
+        self.real_list = self.recursively_read(os.path.join(data_path, '0'))
+        self.fake_list = self.recursively_read(os.path.join(data_path, '1'))
+        self.total_list = self.real_list + self.fake_list
+        random.shuffle(self.total_list)
+
+        self.mean, self.std = self.calculate_mean_std()
+        self.dataset = self.create_dataset()
+        self.dataloader = self.create_dataloader()
+
+    def recursively_read(self, rootdir, exts=["png", "jpg", "JPEG", "jpeg"]):
+        out = []
+        for r, _, f in os.walk(rootdir):
+            for file in f:
+                if file.split('.')[-1].lower() in exts:
+                    out.append(os.path.join(r, file))
+        return out
+
+    def create_dataset(self):
+        return RealFakeDataset(self.total_list, self.cropSize, self.isTrain, self.mean, self.std)
+
+    def create_dataloader(self):
+        if self.isTrain:
+            sampler = self.get_bal_sampler()
+        else:
+            sampler = None
+        return DataLoader(
+            self.dataset,
+            batch_size=self.batch_size,
+            shuffle=(sampler is None),
+            sampler=sampler,
+            num_workers=self.num_threads
+        )
+
+    def get_bal_sampler(self):
+        targets = [0 if '0' in path else 1 for path in self.total_list]
+        class_sample_count = torch.tensor([len(np.where(targets == t)[0]) for t in np.unique(targets)])
+        weight = 1. / class_sample_count.float()
+        samples_weight = torch.tensor([weight[t] for t in targets])
+        return WeightedRandomSampler(samples_weight, len(samples_weight))
+
+    def calculate_mean_std(self):
+        if self.isTrain:
+            transform = transforms.Compose([
+                transforms.Resize(self.cropSize),
+                transforms.ToTensor(),
+            ])
+            dataset = RealFakeDataset(self.total_list, self.cropSize, self.isTrain, transform=transform)
+            dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_threads)
+        else:
+            return torch.tensor([0.485, 0.456, 0.406]), torch.tensor([0.229, 0.224, 0.225])
+
+        mean = 0.0
+        std = 0.0
+        nb_samples = 0
+        for data, _ in dataloader:
+            batch_samples = data.size(0)
+            data = data.view(batch_samples, data.size(1), -1)
+            mean += data.mean(2).sum(0)
+            std += data.std(2).sum(0)
+            nb_samples += batch_samples
+
+        mean /= nb_samples
+        std /= nb_samples
+        return mean, std
+
+class RealFakeDataset(Dataset):
+    def __init__(self, file_list, cropSize, isTrain, mean=None, std=None, transform=None):
+        self.file_list = file_list
+        self.cropSize = cropSize
+        self.isTrain = isTrain
+        self.mean = mean
+        self.std = std
+        if transform is None:
+            if self.isTrain:
+                self.transform = transforms.Compose([
+                    transforms.RandomResizedCrop(self.cropSize),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=self.mean, std=self.std),
+                ])
+            else:
+                self.transform = transforms.Compose([
+                    transforms.Resize(self.cropSize),
+                    transforms.CenterCrop(self.cropSize),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=self.mean, std=self.std),
+                ])
+        else:
+            self.transform = transform
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, idx):
+        img_path = self.file_list[idx]
+        label = 0 if '0' in img_path else 1
+        img = Image.open(img_path).convert("RGB")
+        img = self.transform(img)
+        return img, label
+
+# 使用示例
+data_path = 'data'
+cropSize = 256
+batch_size = 32
+num_threads = 4
+
+# 初始化数据加载器
+data_loader = RealFakeDataLoader(data_path, cropSize, batch_size, num_threads)
+
+# 打印数据加载器中的数据
+for i, (img, label) in enumerate(data_loader):
+    print(f"Image {i}: {img.shape}, Label: {label}")
+    if i == 5:
+        break
