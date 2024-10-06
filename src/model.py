a058367871767826816defc8ad5ca5936403b6aa
louzongzhi <2871561809@qq.com>
2024-10-06 17:58:43 +0800

    重构代码以提高模型定义的可读性和可维护性

--- src/model.py
+++ src/model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torchvision.models import resnet50, resnet101, resnet152
+from torchvision.models import resnet152
 
 
 class self_net(nn.Module):
@@ -8,7 +8,7 @@ class self_net(nn.Module):
         super(self_net, self).__init__()
         self.num_classes = num_classes
 
-        self.backbone = resnet101(pretrained=True)
+        self.backbone = resnet152(pretrained=True)
         self.fc = nn.Linear(2048, num_classes)
 
     def forward(self, x):
