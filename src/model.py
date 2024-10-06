a582d63dccff296b5b5c4817dc158c11f23e08e5
louzongzhi <2871561809@qq.com>
2024-10-06 17:51:49 +0800

    重构代码以提高模型定义的可读性和可维护性

--- src/model.py
+++ src/model.py
@@ -8,7 +8,7 @@ class self_net(nn.Module):
         super(self_net, self).__init__()
         self.num_classes = num_classes
 
-        self.backbone = resnet50(pretrained=True)
+        self.backbone = resnet101(pretrained=True)
         self.fc = nn.Linear(2048, num_classes)
 
     def forward(self, x):
