38794e7e331f3a8f8b6953cc3a173e1001544035
louzongzhi <2871561809@qq.com>
2024-10-09 17:49:31 +0800

    重构代码以提高模型定义的可读性和可维护性

--- src/1、mean_std.py
+++ src/1、mean_std.py
@@ -1,27 +1,22 @@
 from torchvision.datasets import ImageFolder
+import torch
 from torchvision import transforms
-from torch.utils.data import DataLoader
 
 def get_mean_and_std(train_data):
-    train_loader = DataLoader(
-        train_data, batch_size=64, shuffle=False, num_workers=4,
+    train_loader = torch.utils.data.DataLoader(
+        train_data, batch_size=1, shuffle=False, num_workers=0,
         pin_memory=True)
-    mean = 0.0
-    std = 0.0
-    for images, _ in train_loader:
-        batch_mean = images.mean(dim=[0, 2, 3])
-        batch_std = images.std(dim=[0, 2, 3])
-        mean += batch_mean * images.size(0)
-        std += batch_std * images.size(0)
-    mean /= len(train_data)
-    std /= len(train_data)
-    return mean.tolist(), std.tolist()
+    mean = torch.zeros(3)
+    std = torch.zeros(3)
+    for X, _ in train_loader:
+        for d in range(3):
+            mean[d] += X[:, d, :, :].mean()
+            std[d] += X[:, d, :, :].std()
+    mean.div_(len(train_data))
+    std.div_(len(train_data))
+    return list(mean.numpy()), list(std.numpy())
+
 
 if __name__ == '__main__':
-    transform = transforms.Compose([
-        transforms.ToTensor()
-    ])
-    train_dataset = ImageFolder(root=r'data/', transform=transform) # 替换为测试集再跑一次，分别记录
-    mean, std = get_mean_and_std(train_dataset)
-    print(f"Mean: {mean}")
-    print(f"Std: {std}")
+    train_dataset = ImageFolder(root=r'data/', transform=transforms.ToTensor())
+    print(get_mean_and_std(train_dataset))
